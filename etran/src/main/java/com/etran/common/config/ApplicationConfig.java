c387c679dc4a170886e3611a73aecb46a779b093
dsy <dingshiyang@126.com>
2015-06-03 10:24:14 +0800

    test 注释

--- etran/src/main/java/com/etran/common/config/ApplicationConfig.java
+++ etran/src/main/java/com/etran/common/config/ApplicationConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 
-
+//test 注释
 @Configuration 
 public class ApplicationConfig {
 
